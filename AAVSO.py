ae37c77078048d91831d7c3b24b5cdf2240802b4
cumminj1 <cumminj1@tcd.ie>
2018-09-17 17:07:44 +0100

    PDM Experimentation

--- AAVSO.py
+++ AAVSO.py
@@ -9,11 +9,12 @@ Last edited Thur Sep 13 2018
 
 @author: jeaic
 """
+
 #import the relevant packages
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
-
+from PyAstronomy.pyTiming import pyPDM
 
 # pull the date and magnitude into lists
 AAVSO1= pd.read_excel('AAVSO.xlsx', sheetname='AAVSO_filter')
@@ -79,10 +80,59 @@ print (occur_filt)
 
 
 
-
+"""
 #plot the cleaned up data
 plt.plot(mod_jul_filt,mag_filt,'r.')
 plt.xlabel('modified julian date')
 plt.ylabel('approx vis magnitude')
 plt.title(" AAVSO data on ZUMa using top " + str(no) +  " observers' data")
 plt.show()
+"""
+# Get a ``scanner'', which defines the frequency interval to be checked.
+# Alternatively, also periods could be used instead of frequency.
+S = pyPDM.Scanner(minVal=0.5, maxVal=250.0, dVal=1, mode="frequency")
+
+# Carry out PDM analysis. Get frequency array
+# (f, note that it is frequency, because the scanner's
+# mode is ``frequency'') and associated Theta statistic (t).
+# Use 10 phase bins and 3 covers (= phase-shifted set of bins).
+P = pyPDM.PyPDM(mod_jul_filt, mag_filt)
+#f1, t1 = P.pdmEquiBinCover(3, 6, S)
+# For comparison, carry out PDM analysis using  bins (no covers).
+f2, t2 = P.pdmEquiBin(100, S)
+
+
+
+
+#plot the cleaned up data
+plt.subplot(2,1,1)
+plt.plot(mod_jul_filt,mag_filt,'r.')
+plt.xlabel('modified julian date')
+plt.ylabel('approx vis magnitude')
+plt.title("AAVSO recent data on ZUMa")
+
+plt.subplot(2,1,2)
+plt.title("Result of PDM analysis ")
+plt.xlabel("Frequency")
+plt.ylabel("Theta")
+#plt.plot(f1, t1, 'bp-')
+plt.plot(f2, t2, 'gp-')
+#plt.xticks(numpy.arange(min(f2)+0.5, max(f2)+1.5,1.0))
+plt.legend([" Bins without covers"])
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- testplot_afoev.py
+++ testplot_afoev.py
@@ -10,7 +10,9 @@ Created on Wed Sep 12 17:53:56 2018
 #import the relevant packages
 import pandas as pd
 import matplotlib.pyplot as plt
-from collections import Counter
+#from collections import Counter
+from PyAstronomy.pyTiming import pyPDM
+import numpy 
 
 
 # pull the date and magnitude into lists
@@ -21,16 +23,40 @@ mag=afoev['Visual magnitude']
 #we need to see the fairweather observers
 #use counter to determine the number of times
 #observer has taken measurements
-occur=Counter(afoev['Observer'])
-print (occur)
+#occur=Counter(afoev['Observer'])
+#print (occur)
 #this allows us to remove inconsistent observers
+
+
+# Get a ``scanner'', which defines the frequency interval to be checked.
+# Alternatively, also periods could be used instead of frequency.
+S = pyPDM.Scanner(minVal=0.5, maxVal=250.0, dVal=.1, mode="frequency")
+
+# Carry out PDM analysis. Get frequency array
+# (f, note that it is frequency, because the scanner's
+# mode is ``frequency'') and associated Theta statistic (t).
+# Use 10 phase bins and 3 covers (= phase-shifted set of bins).
+P = pyPDM.PyPDM(mod_jul, mag)
+#f1, t1 = P.pdmEquiBinCover(3, 6, S)
+# For comparison, carry out PDM analysis using  bins (no covers).
+f2, t2 = P.pdmEquiBin(10, S)
 
 
 
 
 #plot the cleaned up data
-plt.plot(mod_jul,mag,'ro')
+plt.subplot(2,1,1)
+plt.plot(mod_jul,mag,'r.')
 plt.xlabel('modified julian date')
 plt.ylabel('approx vis magnitude')
 plt.title("AFOEV recent data on ZUMa")
+
+plt.subplot(2,1,2)
+plt.title("Result of PDM analysis ")
+plt.xlabel("Frequency")
+plt.ylabel("Theta")
+#plt.plot(f1, t1, 'bp-')
+plt.plot(f2, t2, 'gp-')
+#plt.xticks(numpy.arange(min(f2)+0.5, max(f2)+1.5,1.0))
+plt.legend([" Bins without covers"])
 plt.show()

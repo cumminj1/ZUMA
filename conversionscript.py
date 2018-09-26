46dd9a4b96afb2f67019e7bd78d5b7a97a9454fc
cumminj1 <33314825+cumminj1@users.noreply.github.com>
2018-09-26 13:03:30 +0100

    astro room manual overwrite

--- conversionscript.py
+++ conversionscript.py
@@ -7,24 +7,25 @@ from convertdate import julianday
 from convertdate import gregorian
 
 import pandas as pd
+import numpy as np
 
 AAVSO1= pd.read_excel('ZUMa_AFOEV_AAVSO.xlsx', sheet_name='ZUMa_4Mar1920_1Mar2012_aavsodat')
 jul=AAVSO1['JD']
 
-
-#x=julian.monthcalendar(2018,3)
-#print (x)
-
-#est= ju
-
-
+#empty list to store values
 output=[]
-#greg=julian.gregorian_from_jd(jul)
 
+#loop through the data and append the empty list with
+#the converted dates
 for i in range(1,83686):
     result= julianday.to_gregorian(jul[i])
     output.append(result)
     
+#output1=list(output)
+#output1=[n.replace(', ','-')for n in output1]
+#print (output1)
 
-print (output)
+#write the results to a dataframe and then output via csv
+df=pd.DataFrame(np.array(output))
+df.to_csv('time_conversion.csv',encoding='utf-8', sep=' ')
 #print(greg)
